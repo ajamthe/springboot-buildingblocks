fb7083fd630b8868cf305eb17c48e8898a7d4dc3
Ashish Jamthe <ajamthe@gmail.com>
2020-04-24 19:23:57 +1000

    user working

--- src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -2,8 +2,9 @@ package com.stacksimplify.restservices.springbootbuildingblocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication @ComponentScan(basePackages = { "com.stacksimplify.restservices.hello"} )
 public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
