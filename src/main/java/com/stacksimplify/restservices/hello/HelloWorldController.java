0d5c53a8288325b4e170035598cd204110ecbc38
Ashish Jamthe <ajamthe@gmail.com>
2020-04-29 15:08:49 +1000

    i18n - first commit

--- src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
@@ -1,12 +1,16 @@
 package com.stacksimplify.restservices.hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    ResourceBundleMessageSource messageSource;
 
     @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
     @GetMapping("/helloworld")
@@ -18,4 +22,15 @@ public class HelloWorldController {
     public Person getPerson() {
         return Person.builder().firstName("Ash").lastName("Jam").city("Sydney").build();
     }
+
+    @GetMapping("/hello-int")
+    public String getMessagesInI18nFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+        return messageSource.getMessage("label.hello",null, new Locale(locale));
+    }
+
+    @GetMapping("/hello-int2")
+    public String getMessagesInI18nFormat2() {
+        return messageSource.getMessage("label.hello",null, LocaleContextHolder.getLocale());
+    }
+
 }
--- src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -1,13 +1,18 @@
 package com.stacksimplify.restservices.springbootbuildingblocks;
 
 import com.stacksimplify.restservices.exceptions.CustomGlobalExceptionHandler;
-import com.stacksimplify.restservices.exceptions.GlobalRestControllerAdviceExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication @ComponentScan(basePackages = { "com.stacksimplify.restservices.hello",
 		"com.stacksimplify.restservices.services", "com.stacksimplify.restservices.controllers"} )
@@ -19,5 +24,19 @@ public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
